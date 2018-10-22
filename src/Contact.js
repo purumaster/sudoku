1bd6bfddbaca20537575d372d3997eff2e67b38d
Makimi <makimi.hanawa@gmail.com>
2018-10-22 00:03:34 -0400

    I am working on Cotact js.

--- src/Contact.js
+++ src/Contact.js
@@ -1,85 +1,62 @@
 import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+//Externals
+import Field from './Field';
+import Button from './Button';
 
 
-const Contact = props => {
-  const { handleSubmit, pristine, reset, submitting } = props
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>First Name</label>
-        <div>
-          <Field
-            name="firstName"
-            component="input"
-            type="text"
-            placeholder="First Name"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Last Name</label>
-        <div>
-          <Field
-            name="lastName"
-            component="input"
-            type="text"
-            placeholder="Last Name"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Email</label>
-        <div>
-          <Field
-            name="email"
-            component="input"
-            type="email"
-            placeholder="Email"
-          />
-        </div>
-      </div>
-      <div>
-        <label>Sex</label>
-        <div>
-          <label>
+class Contact extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      name:'',
+      email:'',
+      message:''
+
+    };
+    // To ensure 'this' when calling 'this.updateField' refers to Form and not Filed, we do.
+    this.updateField = this.updateField.bind(this);
+  }
+
+   //Field could be 'name','email' or 'message'
+   //Value type whatever the user types into the input field.
+   updateField(field,value){
+     this.setState({ [field]: value})
+
+   }
+
+    render() {
+        return (
+          <div>
+            {/* Name field */}
             <Field
-              name="sex"
-              component="input"
-              type="radio"
-              value="male"
-            />{' '}
-            Male
-          </label>
-          <label>
+              label="Name"
+              onChange={(event)=> this.updateField('name',event.target.value)}
+              value={this.state.name}
+              />
+            {/* Email field */}
             <Field
-              name="sex"
-              component="input"
-              type="radio"
-              value="female"
-            />{' '}
-            Female
-          </label>
-        </div>
-      </div>
-      <div>
-        <label>Notes</label>
-        <div>
-          <Field name="notes" component="textarea" />
-        </div>
-      </div>
-      <div>
-        <button type="submit" disabled={pristine || submitting}>
-          Submit
-        </button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>
-          Clear Values
-        </button>
-      </div>
-    </form>
-  )
+              label="Email"
+              onChange={(event)=> this.updateField('email',event.target.value)}
+              value={this.state.email}
+            />
+            {/* Message textarea */}
+            <Field
+              label="Message"
+              onChange={(event)=> this.updateField('message',event.target.value)}
+              /*This Should be written like 'textarea' */
+              textarea={true}
+              value={this.state.message}
+            />
+            {/* Submit button */}
+            <Button
+            email="makimi.hanawa@gmail.com"
+            formValues={this.state}
+            />
+          </div>
+
+        );
+    }
 }
-
 
 
 export default Contact;
