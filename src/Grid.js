f988325f0e49e773f8c58f4b0a414864ef43fb22
Makimi <makimi.hanawa@gmail.com>
2018-11-12 19:20:40 -0500

    app.css

--- src/Grid.js
+++ src/Grid.js
@@ -4,34 +4,106 @@ import React, { Component } from "react";
 class Grid extends Component {
   render() {
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th className="col-md-3">Invoice</th>
-            <th className="col-md-3">Date</th>
-            <th className="col-md-3">Amount</th>
-            <th className="col-md-3">Status</th>
-            <th className="col-md-3">Invoice</th>
-            <th className="col-md-3">Date</th>
-            <th className="col-md-3">Amount</th>
-            <th className="col-md-3">Status</th>
-            <th className="col-md-3">Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>12345</td>
-            <td>Feb 1, 2014</td>
-            <td>$45.00</td>
-            <td>Owing</td>
-          </tr>
-          <tr>
-            <td>67890</td>
-            <td>Jan 30, 2014</td>
-            <td>$19.99</td>
-            <td>Overdue</td>
-          </tr>
-        </tbody>
+      <table className="table table-bordered">
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+        <tr>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
       </table>
     );
   }
---
+++ src/Play0.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Grid from "./Grid";
+import Check from "./Check";
+
+class Play extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <h3 className="display-3">Sudoku</h3>
+        </div>
+        <Grid />;<Check />;
+      </React.Fragment>
+    );
+  }
+}
+
+export default Play;
